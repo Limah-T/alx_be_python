a5e68177483f86f05130b67d1131fe20795c5170
Limah Temitope <limahtechnology@yahoo.com>
2024-12-19 09:38:55 +0100

    Changed operator to operation for checker to approve.

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,12 +1,17 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operator = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operator:
-    case "+": print(f"The result is {num1 + num2}.")
-    case "-": print(f"The result is {num1 - num2}.")
-    case "*": print(f"The result is {num1 * num2}.")
-    case "/" if num2 != 0: print(f"The result is {num1 / num2}.")
-    case "/": print(f"Cannot divide by zero.")
+match operation:
+    case "+": 
+        print(f"The result is {num1 + num2}.")
+    case "-": 
+        print(f"The result is {num1 - num2}.")
+    case "*": 
+        print(f"The result is {num1 * num2}.")
+    case "/" if num2 != 0: 
+        print(f"The result is {num1 / num2}.")
+    case "/": 
+        print("Cannot divide by zero.")
     case _:
         print("Invalid operator.")

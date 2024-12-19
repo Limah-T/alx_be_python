1c0ffc2f00980a111dbf90b133fc51b21c463998
Limah Temitope <limahtechnology@yahoo.com>
2024-12-19 09:31:59 +0100

    Created a match_case_calculator.py script that accepts numbers from users and performs calculations

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+
+match operator:
+    case "+": print(f"The result is {num1 + num2}.")
+    case "-": print(f"The result is {num1 - num2}.")
+    case "*": print(f"The result is {num1 * num2}.")
+    case "/" if num2 != 0: print(f"The result is {num1 / num2}.")
+    case "/": print(f"Cannot divide by zero.")
+    case _:
+        print("Invalid operator.")

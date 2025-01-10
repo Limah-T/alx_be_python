ee35e424d5f5e34edba759aa4410e67764881a9d
Limah Temitope <limahtechnology@yahoo.com>
2025-01-10 12:31:53 +0100

    Task 1: Created shopping_list_manager.py file that contains display_menu function for user to select choices of adding, removing, viewing list, alse exit.

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,12 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def check_empty_list(shopping_list_menu):
+    if shopping_list_menu == []:
+        print("Shopping list is empty!")
+        return True
+    return False

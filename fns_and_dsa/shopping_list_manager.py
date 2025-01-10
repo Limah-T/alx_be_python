b8c3bd6216e5a54d7098d87951aae4fdc8ed7a3a
Limah Temitope <limahtechnology@yahoo.com>
2025-01-10 12:40:10 +0100

    Moved the code from main.py to shopping_list_manager.py file to enable checker to run the file.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -11,3 +11,35 @@ def check_empty_list(shopping_list_menu):
         print("Shopping list is empty!")
         return True
     return False
+
+while True:
+    display_menu()
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        # Prompt for and add an item
+        item_name = input("Enter the item to add: ").lower().strip()
+        shopping_list.append(item_name)
+        print(item_name, "has been added to shopping list successfully.")
+    elif choice == '2':
+        # Prompt for and remove an item
+        if check_empty_list(shopping_list):
+            pass
+        else:
+            item_name = input("Enter item to remove: ").lower().strip()
+            if item_name in shopping_list:
+              shopping_list.remove(item_name)
+              print(item_name, "has been removed from shopping list successfully.")
+            else:
+                print("Item not found in list!")
+    elif choice == '3':
+        # Display the shopping list
+        if check_empty_list(shopping_list):
+            pass
+        else:
+            for item in shopping_list:
+                print(item)
+    elif choice == '4':
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")

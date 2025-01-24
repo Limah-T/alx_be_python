041fa46c76b3bdf10c82baf0afbbed7c0c08dc3d
Limah Temitope <limahtechnology@yahoo.com>
2025-01-24 11:04:23 +0100

    Created class_static_methis_demo.py script that contains uses of static and class methos with arithmetric operations(multiply, add) examples.

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    calculator_type = "Arithmetric Operations"
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculator_type}")
+        return a * b
+    
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
